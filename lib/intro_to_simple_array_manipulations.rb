e3a579a3e69f748413d16c678b657be961115315
Issa Sangare <issabmsangare@hotmail.com>
2020-03-31 14:22:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,7 @@
 def using_concat(my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end 
+
+def using_insert (list_of_programming_language, another_language)
+  list_of_programming_language.insert(another_language[4])
+end 
